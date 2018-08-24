be90fb8d7d56eb6d07141c2d5523c4e0e43a4381
LoppA <xLopacheco@gmail.com>
2018-08-24 00:30:44 -0300

    end

--- Ex1/Adaline.py
+++ Ex1/Adaline.py
@@ -1,16 +1,18 @@
 from math import exp
 import numpy as np
 
+# Funcao sigmoide
 def sigmoid(x):
     return (2 / (1 + exp(-x))) - 1
 
+# Funcao degrau
 def step(x):
     if(x > 0):
         return int(1)
     return int(0)
 
-THETA = 1.0
-ETA = 0.5
+THETA = 1.0     # Constante theta
+ETA = 0.5       # Constate ete, velocidade de aprendizado
 
 class Adaline:
 
@@ -21,12 +23,14 @@ class Adaline:
     def __str__(self):
         return ("Size: " + str(self.w.shape) + "\nW: " + str(self.w))
 
-    def run(self, x, t):
+    # Verificar a saida y dado input x
+    def run(self, x):
         y = np.sum(x * self.w)
         return self.actv(y)
 
+    # Treinar dado input x e saida esperada t
     def train(self, x, t):
-        y = self.run(x, t)
+        y = self.run(x)
         e = t - y
         if(e != 0):
             self.w += ETA*e*x
--- Ex1/ex1.py
+++ Ex1/ex1.py
@@ -1,8 +1,10 @@
 import Adaline as ada
 import numpy as np
 
+# Inicializacao do perceptron
 per = ada.Adaline(25, ada.sigmoid)
 
+# Leitura do input de test para A
 f = open('A_input_test.txt')
 A_input_test = np.ones((3, 26)).astype(np.float64)
 A_input_test[0][0] = 1
@@ -22,6 +24,7 @@ for line in f:
             j+=1
 A_out_test = np.array([1, 1, 1])
 
+# Leitura do input de treino para A
 f = open('A_input_train.txt')
 A_input_train = np.ones((3, 26)).astype(np.float64)
 A_input_train[0][0] = 1
@@ -41,6 +44,7 @@ for line in f:
             j+=1
 A_out_train = np.array([1, 1, 1])
 
+# Leitura do input de test para A invertido
 f = open('notA_input_test.txt')
 notA_input_test = np.ones((3, 26)).astype(np.float64)
 notA_input_test[0][0] = 1
@@ -60,6 +64,7 @@ for line in f:
             j+=1
 notA_out_test = np.array([-1, -1, -1])
 
+# Leitura do input de treino para A invertido
 f = open('notA_input_train.txt')
 notA_input_train = np.ones((3, 26)).astype(np.float64)
 notA_input_train[0][0] = 1
@@ -81,27 +86,33 @@ notA_out_train = np.array([-1, -1, -1])
 
 f.close()
 
+# Algoritmo LMS
 EPS = 1e-9
 err = 3
 while(err > EPS):
     err = 0
 
+    # Treinando com os modelos de A
     n = A_input_train.shape[0]
     for i in range(n):
         differ = per.train(A_input_train[i], A_out_train[i])
         err += differ*differ
 
+    # Treinando com os modelos de A invertido
     n = notA_input_train.shape[0]
     for i in range(n):
         differ = per.train(notA_input_train[i], notA_out_train[i])
         err += differ*differ
 
+# Impressao do perceptron
 print('\n', per, sep='')
 
+# Testando para os casos de teste de A
 n = A_input_test.shape[0]
 for i in range(n):
-    print("A test", i, per.run(A_input_test[i], A_out_test[i]))
+    print("A test", i, per.run(A_input_test[i]))
 
+# Testando para os casos de teste de A invertido
 n = notA_input_test.shape[0]
 for i in range(n):
-    print("Not A test", i, per.run(notA_input_test[i], A_out_test[i]))
+    print("Not A test", i, per.run(notA_input_test[i]))
