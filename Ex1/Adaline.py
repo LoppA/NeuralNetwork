6d4e3cc8676be476235d32bc7a31c924b174f9dd
LoppA <xLopacheco@gmail.com>
2018-08-23 22:53:51 -0300

    Adaline

---
+++ Ex1/Adaline.py
@@ -0,0 +1,32 @@
+from math import exp
+import numpy as np
+
+def sigmoid(x):
+    return (2 / (1 + exp(-x))) - 1
+
+def step(x):
+    if(x > 0):
+        return int(1)
+    return int(0)
+
+THETA = 1.0
+ETA = 0.5
+
+class Adaline:
+
+    def __init__(self, n, fun=sigmoid):
+        self.actv = fun
+        self.w = np.zeros(n+1).astype(np.float64)
+
+    def __str__(self):
+        return ("Size: " + str(self.w.shape) + "\nW: " + str(self.w))
+
+    def train(self, x, t):
+        y = np.sum(x * self.w)
+
+        y = self.actv(y)
+        e = t - y
+        if(e != 0):
+            self.w += ETA*e*x
+            return True
+        return False
---
+++ Ex1/and_adaline.py
@@ -0,0 +1,23 @@
+import Adaline as ada
+import numpy as np
+
+per = ada.Adaline(2, ada.step)
+
+and_input = np.array([ [1, 0, 0],
+                       [1, 0, 1],
+                       [1, 1, 0],
+                       [1, 1, 1] ]).astype(np.float64)
+
+and_output = np.array( [0, 0, 0, 1] ).astype(np.int)
+
+n = and_input.shape[0]
+cnt = 4
+while(cnt != 0):
+    cnt = 0
+
+    for i in range(n):
+        differ = per.train(and_input[i], and_output[i])
+        if(differ):
+            cnt += 1
+
+print('\n', per, sep='')
